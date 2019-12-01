7ad0b31ef0f0efb1e4fcae60cc4f2b969769a6cd
Laura F Dickinson <l@veriny.tf>
2019-12-01 15:52:37 +0000

    Add first basic test.

---
+++ src/test/kotlin/TestInterpreter.kt
@@ -0,0 +1,32 @@
+/*
+ * This file is part of kython.
+ *
+ * kython is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kython is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kython.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package green.sailor.kython.test
+
+import green.sailor.kython.interpreter.KythonInterpreter
+import green.sailor.kython.interpreter.pyobject.PyString
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class TestInterpreter {
+    @Test
+    fun `Test basic assignment`() {
+        val result = KythonInterpreter.testExec("result = \"Hello, world!\"")
+        assertTrue(result is PyString)
+        assertTrue((result as PyString).wrappedString == "Hello, world!")
+    }
+}
