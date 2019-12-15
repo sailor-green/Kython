d0da1ac0f332989788c568aec1eead951c9d9fa1
Laura F Dickinson <l@veriny.tf>
2019-12-15 21:05:37 +0000

    Rework the core of the interpreter into interpreter threads.

---
+++ src/main/kotlin/interpreter/InterpreterThread.kt
@@ -0,0 +1,141 @@
+/*
+ * This file is part of kython.
+ *
+ * kython is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kython is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kython.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package green.sailor.kython.interpreter
+
+import green.sailor.kython.MakeUp
+import green.sailor.kython.interpreter.pyobject.PyObject
+import green.sailor.kython.interpreter.stack.StackFrame
+import kotlin.system.exitProcess
+
+/**
+ * Represents an interpreter thread.
+ */
+class InterpreterThread(
+    /** The root stack frame for this thread. */
+    var rootStackFrame: StackFrame
+) : Runnable {
+    // lazy to avoid creating a new thread for the main thread, when it's not needed.
+    /** The wrapped Java thread object. */
+    val thread by lazy { Thread(this) }
+
+    /** The current executing stack frame for this thread. */
+    var currentStackFrame: StackFrame? = null
+
+    fun start() = thread.start()
+
+    override fun run() {
+        KythonInterpreter.interpreterThreadLocal.set(this)
+        runStackFrame(rootStackFrame, mapOf())
+    }
+
+    /**
+     * Runs a stack frame on this thread.
+     */
+    fun runStackFrame(frame: StackFrame, args: Map<String, PyObject>): PyObject {
+        val parent = currentStackFrame
+        parent?.let {
+            frame.parentFrame = it
+            it.childFrame = frame
+        }
+
+        currentStackFrame = frame
+        val result = frame.runFrame(args)
+        frame.parentFrame = null
+
+        parent?.let {
+            it.childFrame = null
+            currentStackFrame = it
+        }
+
+        return result
+    }
+
+    /**
+     * Wraps a root frame to handle otherwise unhandled KyErrors. Use [kickoffThread] instead if
+     * you're invoking the interpreter normally, and [runRootFrame] if you want to just invoke the
+     * interpreter.
+     */
+    fun wrapTraceback(rootFrame: StackFrame, rootThread: Boolean) {
+        try {
+            runStackFrame(rootFrame, mapOf())
+        } catch (e: KyError) {
+            if (MakeUp.debugMode) {
+                throw e
+            }
+
+            // bubbled error, means no user code handled it
+            val thread = Thread.currentThread()
+            System.err.println("Exception in thread ${thread.name}")
+            with(e.wrapped) {
+                System.err.println("\nKython stack (most recent frame last):")
+                exceptionFrames.forEach {
+                    System.err.println("   " + it.createStackFrameInfo().tracebackString)
+                }
+
+                val errorString = args.subobjects.joinToString(" ") {
+                    it.getPyStringSafe().wrappedString
+                }
+                System.err.println("${type.name}: $errorString")
+            }
+
+            // exit with status code 1 if we hit an error
+            if (rootThread) {
+                exitProcess(1)
+            }
+        }
+    }
+
+    /**
+     * Kicks off an interpreter thread.
+     *
+     * @param child: If this is a child thread. If false, exceptions will be fatal.
+     */
+    fun kickoffThread(frame: StackFrame, root: Boolean = true) {
+        try {
+            wrapTraceback(frame, root)
+        } catch (e: Throwable) { // blah blah, bad practice, who cares
+            System.err.println("Fatal interpreter error!")
+            e.printStackTrace(System.err)
+            System.err.println("\nKython stack (most recent frame first):")
+
+            val stacks = StackFrame.flatten(frame).reversed()
+            stacks.forEachIndexed { idx, it ->
+                System.err.println("Frame $idx:")
+                with(it.createStackFrameInfo()) {
+                    System.err.println("   $tracebackString")
+                    disassembly?.let { dis ->
+                        System.err.println("\nDisassembly:\n$dis")
+                    }
+                    stack?.let { stack ->
+                        val size = stack.size
+                        if (size != 0) {
+                            System.err.println("Function stack, size: ${stack.size}")
+                            stack.forEachIndexed { index, pyObject ->
+                                System.err.println("    $index: $pyObject")
+                            }
+                        } else {
+                            System.err.println("Function stack is empty")
+                        }
+                    }
+                }
+                // newline
+                System.err.println()
+            }
+        }
+    }
+}
--- src/main/kotlin/interpreter/KythonInterpreter.kt
+++ src/main/kotlin/interpreter/KythonInterpreter.kt
@@ -19,7 +19,6 @@
 
 package green.sailor.kython.interpreter
 
-import green.sailor.kython.MakeUp
 import green.sailor.kython.interpreter.functions.PyUserFunction
 import green.sailor.kython.interpreter.kyobject.KyModule
 import green.sailor.kython.interpreter.pyobject.PyObject
@@ -29,35 +28,58 @@ import green.sailor.kython.util.CPythonCompiler
 import java.nio.file.Files
 import java.nio.file.Path
 
-// todo: config params?
-
 /**
- * Represents the main interpreter object. A number of properties for this object are exposed under the `sys` module.
- * There is only one instance of this; interpreters on threads are ran with a raw StackFrame object on a thread.
+ * Represents the main interpreter object. A number of properties for this object are exposed under
+ * the `sys` module.
  *
+ * This is a singleton encapsulating several [InterpreterThread] objects.
  */
 object KythonInterpreter {
     /** The CPython compiler backend. */
     val cpyInterface = CPythonCompiler()
 
-    /** The root frame thread local storage for each thread. */
-    val rootFrameLocal = ThreadLocal<StackFrame>()
-
-    /** The current stack frame for each thread. */
-    val currentStackFrameLocal = ThreadLocal<StackFrame>()
-
     /** The mapping of modules. */
     val modules = mutableMapOf<String, KyModule>()
+
+    /** The thread-local for the current [InterpreterThread]. */
+    val interpreterThreadLocal = ThreadLocal<InterpreterThread>()
+
+    /**
+     * The list of threads for this interpreter.
+     *
+     * Index 0 is always the main thread object.
+     */
+    val threads = mutableListOf<InterpreterThread>()
 
     /**
      * Gets the root frame for this thread.
      */
-    fun getRootFrameForThisThread(): StackFrame = rootFrameLocal.get()
+    fun getRootFrameForThisThread(): StackFrame = interpreterThreadLocal.get().rootStackFrame
 
     /**
      * Gets the current stack frame for this thread.
      */
-    fun getCurrentFrameForThisThread(): StackFrame = currentStackFrameLocal.get()
+    fun getCurrentFrameForThisThread(): StackFrame =
+        interpreterThreadLocal.get().currentStackFrame
+            ?: error("There is nothing running on this thread?")
+
+    /**
+     * Runs a stack frame.
+     */
+    fun runStackFrame(frame: StackFrame, args: Map<String, PyObject>): PyObject {
+        val thread = interpreterThreadLocal.get()
+        return thread.runStackFrame(frame, args)
+    }
+
+    /**
+     * Runs the main thread.
+     */
+    fun runMainThread(frame: StackFrame) {
+        val interpreterThread = InterpreterThread(frame)
+        threads.add(interpreterThread)
+        // dont kick off a separate thread
+        interpreterThread.run()
+    }
 
     /**
      * Runs Python code from a file.
@@ -70,7 +92,7 @@ object KythonInterpreter {
         module.attribs["__name__"] = PyString("__main__")
         modules["__main__"] = module
 
-        kickoffThread(module.stackFrame, child = false)
+        runMainThread(module.stackFrame)
     }
 
     /**
@@ -83,7 +105,7 @@ object KythonInterpreter {
         val module = KyModule(rootFunction, "<code>", s.split(System.lineSeparator()))
         modules["__main__"] = module
 
-        kickoffThread(module.stackFrame, child = false)
+        runMainThread(module.stackFrame)
     }
 
     /**
@@ -96,108 +118,5 @@ object KythonInterpreter {
     fun buildModule(moduleFunction: PyUserFunction, sourcePath: Path): KyModule {
         return KyModule(moduleFunction, sourcePath.toString(), Files.readAllLines(sourcePath))
             .also { runStackFrame(it.stackFrame, mapOf()) }
-    }
-
-    /**
-     * Runs a stack frame.
-     */
-    fun runStackFrame(frame: StackFrame, args: Map<String, PyObject>): PyObject {
-        val parent = currentStackFrameLocal.get()
-        parent?.let {
-            frame.parentFrame = it
-            it.childFrame = frame
-        }
-
-        currentStackFrameLocal.set(frame)
-        val result = frame.runFrame(args)
-        frame.parentFrame = null
-
-        parent?.let {
-            it.childFrame = null
-            currentStackFrameLocal.set(it)
-        }
-
-        return result
-    }
-
-    /**
-     * Runs the root frame of a thread.
-     */
-    fun runRootFrame(frame: StackFrame): PyObject {
-        rootFrameLocal.set(frame)
-        val result = runStackFrame(frame, mapOf())
-        rootFrameLocal.remove()
-        return result
-    }
-
-    /**
-     * Wraps a root frame to handle otherwise unhandled KyErrors. Use [kickoffThread] instead if
-     * you're invoking the interpreter normally, and [runRootFrame] if you want to just invoke the
-     * interpreter.
-     */
-    fun wrapKythonThread(rootFrame: StackFrame) {
-        try {
-            runRootFrame(rootFrame)
-        } catch (e: KyError) {
-            if (MakeUp.debugMode) {
-                throw e
-            }
-            // bubbled error, means no user code handled it
-            with(e.wrapped) {
-                System.err.println("\nKython stack (most recent frame last):")
-                exceptionFrames.forEach {
-                    System.err.println("   " + it.createStackFrameInfo().tracebackString)
-                }
-
-                val errorString = args.subobjects.joinToString(" ") {
-                    it.getPyStringSafe().wrappedString
-                }
-                System.err.println("${type.name}: $errorString")
-            }
-        }
-    }
-
-    /**
-     * Kicks off an interpreter thread.
-     *
-     * @param child: If this is a child thread. If false, exceptions will be fatal.
-     */
-    fun kickoffThread(frame: StackFrame, child: Boolean = true) {
-        try {
-            wrapKythonThread(frame)
-        } catch (e: Throwable) { // blah blah, bad practice, who cares
-
-            System.err.println("Fatal interpreter error!")
-            e.printStackTrace(System.err)
-            System.err.println("\nKython stack (most recent frame first):")
-
-            val stacks = StackFrame.flatten(frame).reversed()
-            stacks.forEachIndexed { idx, it ->
-                System.err.println("Frame $idx:")
-                with(it.createStackFrameInfo()) {
-                    System.err.println("   $tracebackString")
-                    disassembly?.let { dis ->
-                        System.err.println("\nDisassembly:\n$dis")
-                    }
-                    stack?.let { stack ->
-                        val size = stack.size
-                        if (size != 0) {
-                            System.err.println("Function stack, size: ${stack.size}")
-                            stack.forEachIndexed { index, pyObject ->
-                                System.err.println("    $index: $pyObject")
-                            }
-                        } else {
-                            System.err.println("Function stack is empty")
-                        }
-                    }
-                }
-                // newline
-                System.err.println()
-            }
-        }
-
-        // if (!child) {
-        //    throw e
-        // }
     }
 }
