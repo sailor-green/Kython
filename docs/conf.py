f79d1cf465291c886943fad2abe7e0a08172c264
Laura S Dickinson <l@veriny.tf>
2020-01-28 16:56:30 +0000

    Doc skeleton, no actual docs yet

---
+++ docs/conf.py
@@ -0,0 +1,57 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath("."))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Kython"
+copyright = "2019-2020, sailor.green"
+author = "sailor.green"
+
+# The full version, including alpha/beta/rc tags
+release = "3.9"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
+# ones.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ['_static']
+
+html_css_files = [
+    "css/custom.css",
+]
+
+html_theme_options = {
+    "collapse_navigation": True,
+}
---
+++ docs/index.rst
@@ -0,0 +1,33 @@
+.. Kython documentation master file, created by
+   sphinx-quickstart on Tue Jan 28 15:54:21 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Kython's documentation!
+==================================
+
+Kython is a Python 3.9+ interpreter for the JVM, written in Kotlin.
+
+What is Kython
+--------------
+
+Kython is a bytecode interpreter, which interprets CPython-compatible bytecode and executes it. It
+creates JVM boxed wrappers for Python objects for a seamless Python environment.
+
+What is not Kython
+------------------
+
+Kython is not "Python on the JVM" - your Python code is not translated to JVM bytecode outside of
+certain optimisations.
+
+Getting Started
+---------------
+
+.. toctree::
+   :maxdepth: 2
+   :caption: User Guide
+
+.. toctree::
+    :maxdepth: 2
+    :caption: Dev Guide
+
