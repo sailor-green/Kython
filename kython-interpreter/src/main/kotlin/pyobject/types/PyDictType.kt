5068c47b5b2bb76e0f15f34d0050413d86450e90
Laura S Dickinson <l@veriny.tf>
2020-02-02 14:43:33 +0000

    Implement dict.update.

--- kython-interpreter/src/main/kotlin/pyobject/types/PyDictType.kt
+++ kython-interpreter/src/main/kotlin/pyobject/types/PyDictType.kt
@@ -17,16 +17,52 @@
 
 package green.sailor.kython.interpreter.pyobject.types
 
+import green.sailor.kython.annotation.*
+import green.sailor.kython.interpreter.callable.EMPTY
 import green.sailor.kython.interpreter.pyobject.PyDict
+import green.sailor.kython.interpreter.pyobject.PyNone
 import green.sailor.kython.interpreter.pyobject.PyObject
 import green.sailor.kython.interpreter.pyobject.PyType
+import green.sailor.kython.interpreter.toNativeList
+import green.sailor.kython.interpreter.util.cast
 
 /**
  * Represents the type of a dict.
  */
+@GenerateMethods
 object PyDictType : PyType("dict") {
     override fun newInstance(kwargs: Map<String, PyObject>): PyObject {
         // another simple passthrough
         return PyDict.fromAnyMap(kwargs)
     }
+
+    /** dict.update */
+    @ExposeMethod("update")
+    @MethodParams(
+        MethodParam("self", "POSITIONAL"),
+        MethodParam("other_mapping", "POSITIONAL"),
+        MethodParam("kwargs", "KEYWORD_STAR"),
+        defaults = [Default("other_mapping", EMPTY::class)]
+    )
+    fun pyDictUpdate(kwargs: Map<String, PyObject>): PyObject {
+        val self = kwargs["self"].cast<PyDict>()
+        val other = kwargs["other_mapping"] ?: error("Built-in signature mismatch!")
+
+        // optimisation case
+        if (other is PyDict) {
+            self.items.putAll(other.items)
+        } else {
+            val keys = other.pyIter().toNativeList()
+            for (key in keys) {
+                val item = other.pyGetItem(key)
+                self.items[key] = item
+            }
+        }
+
+        val fnKwargs = kwargs["kwargs"].cast<PyDict>()
+        self.items.putAll(fnKwargs.items)
+
+        return PyNone
+    }
+
 }
